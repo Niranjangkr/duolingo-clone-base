80c14e007b0733f42660935a43f9de9accb77212
Niranjan Gaonkar <niranjangaonkar.09@gmail.com>
2024-05-18 08:14:46 +0530

    updated env

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,7 +5,7 @@ import { NextRequest } from 'next/server'
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY || ''
+  apiKey: process.env.OPENAI_API_KEY || ''
 })
 
 // IMPORTANT! Set the runtime to edge
